adfc6e9678c2852fb75245b39f5a6d0944f76285
tiranusauro <105762913+tiranusauro@users.noreply.github.com>
2022-05-22 08:51:16 -0300

    just download, no search

--- downloader.py
+++ downloader.py
@@ -10,21 +10,16 @@ requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("model", help="Model name to be downloaded")
+parser.add_argument(
+    "media_quantity", help="Quantity of media to be downloaded")
 args = parser.parse_args()
 
-search_url = 'https://fapello.com//search/' + args.model
+model_url = 'https://fapello.com/content/b/e/' + \
+    args.model + '/1000/' + args.model
 
-response = requests.get(search_url)
-match = re.search(
-    r'https:\/\/fapello.com\/content\/t\/a.*\d{4}.jpg', response.text)
-
-model_url = match.group().split('_')[0]
-model_max_media = match.group().split('_')[1]
-model_max_media = model_max_media.split('.')[0]
-
-for i in range(int(model_max_media), 0, -1):
-    url = model_url + '_' + str(str(i).zfill(4)) + '.jpg'
-    print(url)
-    downloaded_file = requests.get(url)
-    file_name = url[url.rindex('/')+1:]
+for i in range(int(args.media_quantity), 0, -1):
+    complete_url = model_url + '_' + str(str(i).zfill(4)) + '.jpg'
+    print(complete_url)
+    downloaded_file = requests.get(complete_url)
+    file_name = complete_url[complete_url.rindex('/')+1:]
     open(file_name, 'wb').write(downloaded_file.content)
